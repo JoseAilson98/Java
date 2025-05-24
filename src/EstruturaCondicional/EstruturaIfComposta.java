7c8beb7768e89c0a7cb0519f7dff416d3afc67b3
José Ailson <joseailson1998@gmail.com>
2025-05-24 18:03:01 -0300

    if else

---
+++ src/EstruturaCondicional/EstruturaIfComposta.java
@@ -0,0 +1,15 @@
+package EstruturaCondicional;
+
+public class EstruturaIfComposta {
+    public static void main(String[] args) {
+
+        int a = 10;
+
+        if(a >= 5){
+            System.out.println("a é maior");
+        }
+        else {
+            System.out.println("a não é maior");
+        }
+    }
+}
