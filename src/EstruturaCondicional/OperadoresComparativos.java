ea5322601c1f46e166075a1c64a722b01407e662
José Ailson <joseailson1998@gmail.com>
2025-05-24 17:44:35 -0300

    Expressões comparativas

---
+++ src/EstruturaCondicional/OperadoresComparativos.java
@@ -0,0 +1,30 @@
+package EstruturaCondicional;
+
+public class OperadoresComparativos {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+
+        // maior
+        System.out.println(a > b);
+
+        //menor
+        System.out.println(a < b);
+
+        //maior ou igual
+        System.out.println(a >= b);
+
+        //menor ou igual
+        System.out.println(a <= b);
+
+        //igual
+        System.out.println(a == b);
+
+        //diferente
+        System.out.println(a != b);
+
+
+    }
+}
