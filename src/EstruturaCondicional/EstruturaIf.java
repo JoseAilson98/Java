ffa8ffebe83800afd4e66c795313ec66fe6791ab
José Ailson <joseailson1998@gmail.com>
2025-05-24 17:58:49 -0300

    if simples

---
+++ src/EstruturaCondicional/EstruturaIf.java
@@ -0,0 +1,13 @@
+package EstruturaCondicional;
+
+public class EstruturaIf {
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 5 ;
+
+        if(x >= y){
+            System.out.println("X é maior ou igual y!");
+        }
+    }
+}
