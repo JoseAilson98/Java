8a3223fd54fd8ee330306426bcf4c7065b605348
José Ailson <joseailson1998@gmail.com>
2025-05-25 13:54:28 -0300

    Estrutura de For "Para"

---
+++ src/EstruturaRepetitivas/EstruturaFor.java
@@ -0,0 +1,15 @@
+package EstruturaRepetitivas;
+
+public class EstruturaFor {
+    public static void main(String[] args) {
+
+        int numero = 11;
+
+
+        for (int i = 1; i < numero; i++){
+
+            System.out.println(i);
+        }
+
+    }
+}
