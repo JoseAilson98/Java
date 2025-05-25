4f1083ac281bf2cd72f84271173c172385d853de
José Ailson <joseailson1998@gmail.com>
2025-05-25 12:34:38 -0300

    Expressão Ternária

---
+++ src/EstruturaCondicional/EstruturaTernaria.java
@@ -0,0 +1,12 @@
+package EstruturaCondicional;
+
+public class EstruturaTernaria {
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 50;
+
+        String resultado = (x > y ) ? "sim" : "nao";
+        System.out.println(resultado);
+    }
+}
